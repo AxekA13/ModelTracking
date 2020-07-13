c7c2e98c66a2d66805e45d74a309482fb6dffd10
AxekA13 <nastya.trosman@mail.ru>
2020-07-13 23:13:03 +0300

    Created new file for scp

--- scp.py
+++ scp.py
@@ -2,14 +2,13 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='Rsync argument parser')
-parser.add_argument('-sf','--source_folder',help='source synchronizing folder',default='root@127.0.0.1:/colabdrive/mlruns/')
+parser.add_argument('-sf','--source_folder',help='source synchronizing folder',default='root@2.tcp.ngrok.io:/content/NLP_Emotions/mlruns')
 parser.add_argument('-tf','--target_folder',help='target synchronizing folder',default='/home/axeka/VSCodeProjects/NLP_Emotions/NLP_Emotions/mlruns')
 parser.add_argument('-p','--port',help='port for connection',default='9999')
 args = parser.parse_args()
 
-print(f'rsync -zavP "-e ssh -p {args.port}"{args.source_folder} {args.target_folder}')
-os.system(f'rsync -zaP "-e ssh -p {args.port}"root@127.0.0.1:{args.source_folder} {args.target_folder}')
-os.system('mlflow ui')
+print(f'scp -r -P {args.port} {args.source_folder} {args.target_folder}')
+os.system(f'scp -r -P {args.port} {args.source_folder} {args.target_folder}')
 
 
 
