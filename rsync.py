bfb6b75d764b4d51e639d7c736bbbaa6fdaf3f8c
AxekA13 <nastya.trosman@mail.ru>
2020-07-13 05:32:05 +0300

    Added synchronization with ssh

---
+++ rsync.py
@@ -0,0 +1,15 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Rsync argument parser')
+parser.add_argument('-sf','--source_folder',help='source synchronizing folder',default='root@127.0.0.1:/colabdrive/mlruns/')
+parser.add_argument('-tf','--target_folder',help='target synchronizing folder',default='/home/axeka/VSCodeProjects/NLP_Emotions/NLP_Emotions/mlruns')
+parser.add_argument('-p','--port',help='port for connection',default='9999')
+args = parser.parse_args()
+
+print(f'rsync -zavP "-e ssh -p {args.port}"{args.source_folder} {args.target_folder}')
+os.system(f'rsync -zavP "-e ssh -p {args.port}" {args.source_folder} {args.target_folder}')
+os.system('mlflow ui')
+
+
+
