c83c32bbb013cf93286e4016d5cc0f25687ae54c
AxekA13 <nastya.trosman@mail.ru>
2020-07-24 00:27:01 +0000

    Added a commit time output function

--- git_url.py
+++ git_url.py
@@ -4,3 +4,8 @@ def get_commit_url():
     remote_url = subprocess.check_output("git remote get-url origin",shell=True)
     commit = subprocess.check_output("git rev-parse HEAD",shell=True)
     return str(remote_url).split('.git')[0][2:] + '/tree/' + str(commit)[2:-3]
+
+def get_commit_time():
+    time = subprocess.check_output("git show -s --format=%ci HEAD",shell=True)
+    return time[2:-7]
+print(get_commit_time())
